9717896b640a7d04a44cceee98ce0897df964f77
luto <m@luto.at>
2019-01-26 12:13:19 +0100

    add management command to import STL

---
+++ src/crowdprinter/management/commands/importstl.py
@@ -0,0 +1,36 @@
+import argparse
+import os.path
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.files.base import ContentFile, File
+from django.core.files.storage import default_storage
+
+import crowdprinter.models as models
+
+
+class Command(BaseCommand):
+    help = 'add the given STL file to the database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--slug', type=str, required=True)
+        parser.add_argument('--stl-file', type=argparse.FileType(mode='br'), required=True)
+        parser.add_argument('--render-file', type=argparse.FileType(mode='br'))
+
+    def _save_file(self, f, slug, extension):
+        path = default_storage.get_available_name(f'{slug}.{extension}')
+        cfile = ContentFile(f.read())
+        default_storage.save(path, cfile)
+        return path, cfile
+
+
+    def handle(self, *args, **options):
+        slug = options['slug']
+
+        f = models.PrintFile(slug=slug)
+        f.stl.save(*self._save_file(options['stl_file'], slug, 'stl'))
+        if options['render_file']:
+            f.render.save(*self._save_file(
+                options['render_file'],
+                slug,
+                os.path.splitext(options['render_file'].name)[1]
+            ))
