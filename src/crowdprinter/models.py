14629bd75e1dae3e5ed6b3b290c8c73626bfaeba
luto <m@luto.at>
2019-02-03 12:08:35 +0100

    nicen up str()s of models for admin

--- src/crowdprinter/models.py
+++ src/crowdprinter/models.py
@@ -12,9 +12,15 @@ class PrintJob(models.Model):
     def running_attempt(self):
         return self.attempts.order_by('started').filter(ended__isnull=True).get()
 
+    def __str__(self):
+        return f'{self.slug}'
+
 
 class PrintAttempt(models.Model):
     job = models.ForeignKey('PrintJob', on_delete=models.CASCADE, related_name='attempts')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     started = models.DateField(auto_now_add=True)
     ended = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Print Attempt at {self.job}: user={self.user}, ended={self.ended}'
